bdf3636369ae3e3a5997537bca9ce7120123c511
Roland Swingler <roland.swingler@gmail.com>
2012-12-13 13:48:29 +0000

    A recap of the first lesson

---
+++ 01_lesson_1_recap.rb
@@ -0,0 +1,20 @@
+# 1. declare two variables and assign them odd integer values
+
+# 2. write an expression dividing the Task #1 variables, but
+#    displaying the full fractional remainder
+
+# 3. use ** and + in a single expression to display the result 21
+
+# 4. write four expressions, two displaying even numbers modulus 2
+#    and two displaying odd numbers modules 2. notice a pattern?
+
+# 5. Declare a variable called name with your name and a variable
+#    called age with a number. Display them using concatenation
+#    (using +).
+
+# 6. display the same output as above, but this time using string
+#    interpolation.
+
+# 7. do the same as above, but output your name in uppercase. Use
+#    ruby-doc.org or "string".methods in IRB to find a method that
+#    will do this for you.
