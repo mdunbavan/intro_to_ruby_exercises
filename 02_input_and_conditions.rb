feb2f06dd586ab7e9cede5ed7411d71e04a56a56
Roland Swingler <roland.swingler@gmail.com>
2012-12-13 14:38:53 +0000

    Second exercise

---
+++ 02_input_and_conditions.rb
@@ -0,0 +1,19 @@
+# 1. Ask the user to enter their name. Ask the user to enter their
+#    age. Assign both of these to variables. Display the name of the
+#    user and the year they were born in.
+
+
+
+# 2. Use an if statement to output whether a user has a long or short name,
+#    given the name they entered for task 1. Use your_variable.size to
+#    get the length of a string. When the user's name is 3 letters or
+#    under, display "You have a short name". If a user has a name 7
+#    letters or longer, display "You have a long name". Otherwise,
+#    display "Your name is average sized".
+
+
+
+# 3. Ask the user to input a degree value from 0 to 359. Assuming 0 is
+#    North, use "case" and "when" to output whether the degrees point
+#    Northeast, Southeast, Southwest, or Northwest. Remember that
+#    gets.chomp retrieves a string value.
