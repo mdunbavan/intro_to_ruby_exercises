ab2e5d22fcb977e2ecd961508b5ad28b214588bb
mdunbavan <mdunbavan@gmail.com>
2012-12-14 10:14:34 +0000

    All changes made to source files

--- 01_lesson_1_recap.rb
+++ 01_lesson_1_recap.rb
@@ -1,20 +1,43 @@
 # 1. declare two variables and assign them odd integer values
 
+odd = 45
+add2 = 5
+
 # 2. write an expression dividing the Task #1 variables, but
 #    displaying the full fractional remainder
 
+puts odd / add2
+
 # 3. use ** and + in a single expression to display the result 21
+
+puts 2 ** 3 + 13
 
 # 4. write four expressions, two displaying even numbers modulus 2
 #    and two displaying odd numbers modules 2. notice a pattern?
+
+puts 6 % 20
+puts 10 % 100
+
+puts 13 % 10
+puts 37 % 8
 
 # 5. Declare a variable called name with your name and a variable
 #    called age with a number. Display them using concatenation
 #    (using +).
 
+name = 'Mark'
+age = 27.to_s
+
+puts "My name is " + name + " and my age is " + age
+
+
 # 6. display the same output as above, but this time using string
 #    interpolation.
+
+puts "My name is: #{name}".upcase + "\n" + "My ages is: #{age}"
 
 # 7. do the same as above, but output your name in uppercase. Use
 #    ruby-doc.org or "string".methods in IRB to find a method that
 #    will do this for you.
+
+puts "your"
--- 02_input_and_conditions.rb
+++ 02_input_and_conditions.rb
@@ -2,7 +2,19 @@
 #    age. Assign both of these to variables. Display the name of the
 #    user and the year they were born in.
 
-
+# puts "Whats your name:"
+# puts "Whats your age:"
+# puts "Whats your sisters_age:"
+# 
+# name = gets.chomp
+# age = gets.chomp
+# sisters_age = gets.chomp
+# 
+# puts "Your Name is #{name}" + "\n" + "Your age is #{age}"
+# 
+# puts "Hey your name is #{name}" + "\n" + "and guess what you are #{age} years old close to thirty" + "But what about your sister who is #{sisters_age}"
+# 
+# puts "#{name} was born in" + 
 
 # 2. Use an if statement to output whether a user has a long or short name,
 #    given the name they entered for task 1. Use your_variable.size to
@@ -11,6 +23,13 @@
 #    letters or longer, display "You have a long name". Otherwise,
 #    display "Your name is average sized".
 
+bin = "rubbish"
+
+if bin != "rubbish"
+ puts "This bin is #{bin}"
+else
+ puts "This bin is not rubbish"
+end
 
 
 # 3. Ask the user to input a degree value from 0 to 359. Assuming 0 is
