295c00715fb017f53cd46ddeb7bb2918d4ad7af9
Roland Swingler <roland.swingler@gmail.com>
2012-12-17 21:10:17 +0000

    Method exercises

---
+++ 06_methods.rb
@@ -0,0 +1,14 @@
+# 1. Write a method that takes an age (as a number), and returns
+#    the year of birth. Display the output of calling the method with
+#    your age.
+
+
+# 2. Write a method that takes any number of numeric arguments, and
+#    then prints out the month name for each of those numbers on a
+#    single line. i.e. for the arguments 1, 4, 5 the print out should
+#    be "January April May". Remember variable scope and the fact
+#    array indexes start at 0
+
+months = ["January", "February", "March", "April",
+          "May", "June", "July", "August", "September",
+          "October", "November", "December"]
